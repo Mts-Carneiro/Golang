0a96bc5bbcbb507034b100b4b90a33dc9c080bc2
Matheus Carneiro <mtscrn21@gmail.com>
2025-01-20 14:22:27 -0300

    Variaveis boleanas e erros

--- 3 - Tipos de Dados/tipos-de-dados.go
+++ 3 - Tipos de Dados/tipos-de-dados.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main(){
 	var numero int16 = 100
 	fmt.Println(numero)
+
+	var numeroReal1 float32 = 1233.25
+	fmt.Println(numeroReal1)
+
+	var boleano bool
+	fmt.Println(boleano)
+
+	var erro error = errors.New("Erro interno")
+	fmt.Println(erro)
 }
 
