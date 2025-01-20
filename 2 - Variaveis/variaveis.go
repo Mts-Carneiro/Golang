413efc9696701d5c25190f90f7c2b9a27974b458
Matheus Carneiro <mtscrn21@gmail.com>
2025-01-20 14:11:09 -0300

    Aula 2 e 3

---
+++ 2 - Variaveis/variaveis.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main(){
+	var variavel string = "variavel 1"
+	variavel2 := "variavel 2"
+	fmt.Println(variavel)
+	fmt.Println(variavel2)
+}
---
+++ 3 - Tipos de Dados/tipos-de-dados.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	var numero int16 = 100
+	fmt.Println(numero)
+}
+
