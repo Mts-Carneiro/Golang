aefed5a48d715b419961a0f31d31030983ce12e2
Matheus Carneiro <mtscrn21@gmail.com>
2025-01-31 11:46:40 -0300

    Array internos

--- 8 - Array e Slices/arrays_slices.go
+++ 8 - Array e Slices/arrays_slices.go
@@ -24,4 +24,20 @@ func main(){
 
 	slice2 := array2[1:3]
 	fmt.Println(slice2)
+
+	//Arrays internos
+	slice3 := make([]float32,10,11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	fmt.Println(slice3)
+	fmt.Println(cap(slice3))
+	
+	//Quando o slice estrapola o valor limite ele acabaca criando outro array para referenciar com o dobro do tamanho do array anterior
+	slice3 = append(slice3, 5)
+	fmt.Println(slice3)
+	fmt.Println(cap(slice3))
+
+
+
 }
